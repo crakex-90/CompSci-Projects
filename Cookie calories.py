a55a1d30de34b793099c88e6d8f74e4ef9d7b9a7
crakex-90 <80852379+crakex-90@users.noreply.github.com>
2021-09-20 21:50:42 -0700

    Add files via upload

---
+++ Cookie calories.py
@@ -0,0 +1,6 @@
+cookies_eaten = float(input('How many cookies did you eat?'))
+servings_consumed = cookies_eaten / 4
+print('Your total servings were', servings_consumed)
+calories_eaten = float(input('Enter your total servings now.'))
+calories_eaten = servings_consumed * 300
+print('Your total calories were', calories_eaten)
